e93fb7616f0f5e224e60f70772751cac4b1e8449
Nick Smit <nick.smit@gmail.com>
2015-02-20 14:20:23 +0100

    added a toggle button and got the delete button working

--- simple-todos.js
+++ simple-todos.js
@@ -3,45 +3,45 @@ Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
   // This code only runs on the client
-  fetchTasks();
-  addTask();
-  removeTask();
+  Template.body.helpers({
+    fetchTasks();
+  });
+
+  Template.body.events({
+    addTask();
+    removeTask();
+  });
 }
 
 function fetchTasks(){
-  Template.body.helpers({
-    tasks: function(){
-      return Tasks.find({}, {sort: {createdAt: 0}});
-    }
-  });
+  tasks: function(){
+    return Tasks.find({}, {sort: {createdAt: 0}});
+  }
 }
 
 function addTask(){
-  Template.body.events({
-    "submit .new-task": function (event) {
-      // This function is called when the new task form is submitted
-
-      var text = event.target.text.value;
-
-      Tasks.insert({
-        text: text,
-        createdAt: new Date() // current time
-      });
-
-      // Clear form
-      event.target.text.value = "";
-
-      // Prevent default form submit
-      return false;
-    }
-  });
+  "submit .new-task": function (event) {
+    // This function is called when the new task form is submitted
+    var text = event.target.text.value;
+    Tasks.insert({
+      text: text,
+      createdAt: new Date() // current time
+    });
+    // Clear form
+    event.target.text.value = "";
+    // Prevent default form submit
+    return false;
+  }
 }
 
 function removeTask(){
-  Template.body.events({
-    "click .remove-task": function(event){
-      var button = event;
-      console.log(button);
-    }
-  })
+  "click .delete": function(){
+    Tasks.remove(this._id);
+  }
+}
+
+function toggleTask(){
+  "click .toggle-checked": function(event){
+    Tasks.update(this._id, {$set, {checked, !this.checked }});
+  }
 }
