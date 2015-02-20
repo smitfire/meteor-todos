29659732fb6aa07eb887a68c466b070e3a75543f
Nick Smit <nick.smit@gmail.com>
2015-02-20 15:04:19 +0100

    fixed syntax error in toggle function

--- simple-todos.js
+++ simple-todos.js
@@ -3,45 +3,49 @@ Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
   // This code only runs on the client
-  Template.body.helpers({
-    fetchTasks();
-  });
-
-  Template.body.events({
-    addTask();
-    removeTask();
-  });
+  fetchTasks();
+  addTask();
+  removeTask();
+  toggleTask();
 }
 
 function fetchTasks(){
-  tasks: function(){
-    return Tasks.find({}, {sort: {createdAt: 0}});
-  }
+  Template.body.helpers({
+    tasks: function(){
+      return Tasks.find({}, {sort: {createdAt: 0}});
+    }
+  });
 }
 
 function addTask(){
-  "submit .new-task": function (event) {
-    // This function is called when the new task form is submitted
-    var text = event.target.text.value;
-    Tasks.insert({
-      text: text,
-      createdAt: new Date() // current time
-    });
-    // Clear form
-    event.target.text.value = "";
-    // Prevent default form submit
-    return false;
-  }
+  Template.body.event({
+    "submit .new-task": function (event) {
+      // This function is called when the new task form is submitted
+      var text = event.target.text.value;
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+      // Clear form
+      event.target.text.value = "";
+      // Prevent default form submit
+      return false;
+    }
+  });
 }
 
 function removeTask(){
-  "click .delete": function(){
-    Tasks.remove(this._id);
-  }
+  Template.body.event({
+    "click .delete": function(event){
+      Tasks.remove(this._id);
+    }
+  });
 }
 
 function toggleTask(){
-  "click .toggle-checked": function(event){
-    Tasks.update(this._id, {$set, {checked, !this.checked }});
-  }
+  Template.body.event({
+    "click .toggle-checked": function(event){
+      Tasks.update(this._id, {$set: {checked: !this.checked }});
+    }
+  });
 }
