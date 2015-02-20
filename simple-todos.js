9cd9260c396c0dcfbc6bc8e859a90a86a3c1032d
Nick Smit <nick.smit@gmail.com>
2015-02-20 13:38:58 +0100

    changed the template variables back to original names

--- simple-todos.js
+++ simple-todos.js
@@ -1,24 +1,24 @@
 if (Meteor.isClient) {
   // counter starts at 0
-  Session.setDefault('counter', 0);
+  Session.setDefault("counter", 0);
 
-  Template.yolo.helpers({
+  Template.hello.helpers({
     counter: function () {
-      return Session.get('counter');
+      return Session.get("counter");
     }
   });
 
-  Template.yolo.events({
+  Template.hello.events({
     'click button': function () {
       // increment the counter when button is clicked
-      console.log("button pressed");
-      Session.set('counter', Session.get('counter') + 1);
+      Session.set("counter", Session.get("counter") + 1);
     }
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
+    console.log("STARTED THE SERVER YO!");
     // code to run on server at startup
   });
 }
